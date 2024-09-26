5aabfc8c69ff11d9cd170fbc4bb6731258f56b23
officialislandsarchive <officialislandsarchive@gmail.com>
2024-09-25 20:36:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-const colorInput1 = document.getElementById('colorInput1');
-const colorOverlay1 = document.getElementById('colorOverlay1');
+const colorInput = document.getElementById('colorInput');
+const colorOverlay = document.getElementById('colorOverlay');
 
-const colorInput2 = document.getElementById('colorInput2');
-const colorOverlay2 = document.getElementById('colorOverlay2');
-
-colorInput1.addEventListener('input', function () {
-    const selectedColor = colorInput1.value;
-    colorOverlay1.style.backgroundColor = selectedColor;
-    colorOverlay1.style.opacity = '0.5'; 
-});
-
-colorInput2.addEventListener('input', function () {
-    const selectedColor = colorInput2.value;
-    colorOverlay2.style.backgroundColor = selectedColor;
-    colorOverlay2.style.opacity = '0.5'; 
+colorInput.addEventListener('input', function () {
+    const selectedColor = colorInput.value;
+    colorOverlay.style.backgroundColor = selectedColor;
 });
